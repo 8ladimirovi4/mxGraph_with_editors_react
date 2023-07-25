f678c75945da78ccfd1f5b9df41f48a7cb236231
Vladimir Leonov <SESA704696@se.com>
2023-07-25 18:41:15 +0300

    Перенес view.js в отдельный компонент

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -1,171 +1,143 @@
 ﻿/*jshint unused:false,evil:true*/
 
-$(window).on('appready', function (e)
+var init = function ()
+{
+    let editorRoot = '/thirdparty/mxgraph/editor/';
+    
+    // initialization
+    this.mxBasePath = "/thirdparty/mxgraph/";
+    // js path
+    this.JS_PATH = editorRoot;
+
+    // Public global variables
+    this.MAX_REQUEST_SIZE = this.MAX_REQUEST_SIZE || 10485760;
+    this.MAX_AREA = this.MAX_AREA || 15000 * 15000;
+
+    this.RESOURCES_PATH = editorRoot + "res";
+
+    this.RESOURCE_BASE = editorRoot + 'i18n/scheme';
+
+    // disable default resources
+    this.mxLoadResources = false;
+    this.mxLoadStylesheets = false;
+
+    this.mxLanguages = ['ru', 'en', 'fr'];
+    this.mxLanguage = webix.storage.local.get("app.locale");
+    this.mxDefaultLanguage = "en";
+};
+
+var buildViewer = function (scheme)
 {
 
-    if (typeof template !== "object")
-        return;
+    mxResources.loadDefaultBundle = false;
+    var bundle = mxResources.getDefaultBundle(RESOURCE_BASE, mxLanguage) || mxResources.getSpecialBundle(RESOURCE_BASE, mxLanguage);
 
-    webix.ajax('/assets/i18n/' + i18next.language + ".json").then(function (res)
-    {
-        i18next.addResourceBundle(i18next.language, 'assets', res.json(), true);
-        document.title = translate("Scheme");;
-        template.interactive = true;
-       
-        var init = function ()
+    var themes = new Object();
+    mxUtils.getAll([bundle, RESOURCES_PATH + '/default.xml'],
+        function (xhr)
         {
-            let editorRoot = '/thirdparty/mxgraph/editor/';
+            // Adds bundle text to resources
+            mxResources.parse(xhr[0].getText());
+            // Configures the default graph theme
+           
+            themes[Graph.prototype.defaultThemeName] = xhr[1].getDocumentElement();
+          
+            // Build viewer
             
-            // initialization
-            this.mxBasePath = "/thirdparty/mxgraph/";
-            // js path
-            this.JS_PATH = editorRoot;
-
-            // Public global variables
-            this.MAX_REQUEST_SIZE = this.MAX_REQUEST_SIZE || 10485760;
-            this.MAX_AREA = this.MAX_AREA || 15000 * 15000;
-
-            this.RESOURCES_PATH = editorRoot + "res";
-       
-            this.RESOURCE_BASE = editorRoot + 'i18n/scheme';
-
-            // disable default resources
-            this.mxLoadResources = false;
-            this.mxLoadStylesheets = false;
-
-            this.mxLanguages = ['ru', 'en', 'fr'];
-            this.mxLanguage = webix.storage.local.get("app.locale");
-            this.mxDefaultLanguage = "en";
-        };
-        
-        var buildViewer = function (scheme)
+            window.viewer = new EditorUi(new Editor(true, themes), document.getElementById('viewer'), scheme);
+            console.log(window.viewer)
+        },
+        function ()
         {
-            if (!isDefined(scheme))
-                return;
+            messageError("Ошибка загрузки ресурсных файлов.");
+        });
+};
 
-            mxResources.loadDefaultBundle = false;
-            var bundle = mxResources.getDefaultBundle(RESOURCE_BASE, mxLanguage) || mxResources.getSpecialBundle(RESOURCE_BASE, mxLanguage);
+let buildComplete = function (result)
+{
+   
+        webix.ui(result);
+    buildTemplateComplete();
+};
 
-            var themes = new Object();
-            mxUtils.getAll([bundle, RESOURCES_PATH + '/default.xml'],
-                function (xhr)
-                {
-                    // Adds bundle text to resources
-                    mxResources.parse(xhr[0].getText());
-                    // Configures the default graph theme
-                    themes[Graph.prototype.defaultThemeName] = xhr[1].getDocumentElement();
-                    // Build viewer
-                    window.viewer = new EditorUi(new Editor(true, themes), document.getElementById('viewer'), scheme);
-                },
-                function ()
-                {
-                    messageError("Ошибка загрузки ресурсных файлов.");
-                });
-        };
+var buildPageLayout = function (complete)
+{
+    init.apply(window);
 
-        template.buildPageLayout = function (complete)
-        {
-            init.apply(window);
-//---->fix<-------//
-
-            // if (isNullOrEmpty(urlParams['id']))
-            // {
-            //     if (complete)
-            //         complete();
-            //     return;
-            // }
-
-            const id = '2251b1c0-f0a3-4979-99c3-caa251ff3cd2'
-            if (isNullOrEmpty(id))
-            {
-                if (complete)
-                    complete();
-                return;
-            }
-//---->fix<-------//
-            let schemeUI = webix.ui({
-                id: 'contentview',
-                container: "layout",
-                type: "clean",
-                template: "<div class='geViewer' id='viewer'></div>"
-            });
-            webix.require([
-                mxBasePath + 'mxClient.min.js',
-                JS_PATH + 'Init.js',
-                JS_PATH + 'EditorUi.js',
-                JS_PATH + 'Editor.js',
-                JS_PATH + 'Sidebar.js',
-                JS_PATH + 'Graph.js',
-                JS_PATH + 'Shapes.js',
-                JS_PATH + 'Bindings.js',
-                JS_PATH + 'Actions.js',
-                JS_PATH + 'Menus.js',
-                JS_PATH + 'Dialogs.js',
-                JS_PATH + 'ViewMode.js',
-                JS_PATH + 'Scripts.js',
-                JS_PATH + 'MarksService.js',
-            ], function ()
-                {
-                    if (complete)
-                        complete(schemeUI);
-                });
-        };
-
-       
-
-        template.buildTemplateComplete = function ()
-        {
-            
-            if (!window.mxClient)
-            return;
-            
-            if (!mxClient.isBrowserSupported())
-            {
-                messageError('Извините! Ваш браузер не поддерживается.');
-                return;
-            }
-            const LoadGraph = async () => {
-                const response = await  fetch('/assets/1x4kПС-1103510.txt')
-                const text = await response.text()
-                const r = {
-                    id: '2251b1c0-f0a3-4979-99c3-caa251ff3cd2',
-                    model: text,
-                    name:'1x4kПС-1103510',
-                    version: '3.0'
-                }
-                console.log('object')
-                buildViewer.apply(window, [r]);
-            }
-            LoadGraph()
-            // AJAX.get(API.FUNC.schemeGet, 'id=' + urlParams['id'],
-            //     function (xhr, r)
-            //     {
-            //         if (isDefined(r))
-            //             buildViewer.apply(window, [r]);
-            //     },
-            //     function (xhr, err)
-            //     {
-            //         messageError(translate('scheme.errors.load'));
-            //     });
-        };
-
-        var templateDestructor = template.onDestruct;
-        template.onDestruct = function ()
-        {
-            console.log('onDestruct()')
-            templateDestructor();
-            if (window.viewer != null)
-                window.viewer.destroy();
-        };
-
-        try
-        {
-            template.ready();
-        }
-        catch (e)
-        {
-            HELP.log(e);
-            messageError(translate('scheme:errors.init_viewer'));
-        }
+    let schemeUI = webix.ui({
+        id: 'content',
+        container: "layout",
+        type: "clean",
+        template: "<div class='geViewer' id='viewer'></div>",
+     
     });
-});
+    webix.require([
+        '/thirdparty/signalr/signalr.min.js',
+        'thirdparty/mxgraph/deflate/base64.js',
+        'thirdparty/mxgraph/deflate/pako.min.js',
+        'thirdparty/mxgraph/jscolor/jscolor.js',
+        'thirdparty/mxgraph/sanitizer/sanitizer.min.js',
+        'thirdparty/charts/Chart.bundle.min.js',
+        'thirdparty/charts/plugins/chartjs-plugin-annotation.min.js',
+        'thirdparty/charts/Chart.min.css',
+        'css/view.css',
+        'css/scheme.css',
+        'scada.js',
+        mxBasePath + 'mxClient.min.js',
+        JS_PATH + 'Init.js',
+        JS_PATH + 'EditorUi.js',
+        JS_PATH + 'Editor.js',
+        JS_PATH + 'Sidebar.js',
+        JS_PATH + 'Graph.js',
+        JS_PATH + 'Shapes.js',
+        JS_PATH + 'Bindings.js',
+        JS_PATH + 'Actions.js',
+        JS_PATH + 'Menus.js',
+        JS_PATH + 'Dialogs.js',
+        JS_PATH + 'ViewMode.js',
+        JS_PATH + 'Scripts.js',
+        JS_PATH + 'MarksService.js',
+        
+    ], function ()
+        {
+            if (complete)
+           
+         
+                complete(schemeUI);
+        });
+};
+
+
+
+buildTemplateComplete = function ()
+{
+ 
+    if (!window.mxClient)
+    return;
+    
+    if (!mxClient.isBrowserSupported())
+    {
+        messageError('Извините! Ваш браузер не поддерживается.');
+        return;
+    }
+    const LoadGraph = async () => {
+        const response = await  fetch('/assets/1x4kПС-1103510.txt')
+        const text = await response.text()
+       
+        const r = {
+            id: '2251b1c0-f0a3-4979-99c3-caa251ff3cd2',
+            model: text,
+            name:'1x4kПС-1103510',
+            version: '3.0'
+        }
+        buildViewer.apply(window, [r]);
+    }
+    LoadGraph()
+    
+};
+
+    buildPageLayout(buildComplete)
+
+
+
+
--- src/Schemes/Scheme1x4kПС1103510.jsx
+++ src/Schemes/Scheme1x4kПС1103510.jsx
@@ -1,17 +1,119 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as mxgraph from 'mxgraph';
 import './scheme.css';
+import * as webix from 'webix/webix.js';
+const { mxClient, mxUtils, mxResources} = mxgraph();
 
 function Scheme1x4kПС1103510() {
 
+ 
+  const [schemeUI, setSchemeUI] = useState(null);
+  const formRef = useRef();
 
+  useEffect(() => {
 
+   
+    const buildViewer = (scheme) => {
+      mxResources.loadDefaultBundle = false;
+      var bundle =
+        mxResources.getDefaultBundle('/thirdparty/mxgraph/editor/i18n/scheme', ['ru', 'en', 'fr']) ||
+        mxResources.getSpecialBundle('/thirdparty/mxgraph/editor/i18n/scheme', ['ru', 'en', 'fr']);
 
-  return (
-    <div>
-  <div id="layout" className="layout"></div>
-    </div>
-  )
-}
+      var themes = new Object();
+      mxUtils.getAll([bundle, '/thirdparty/mxgraph/editor/res/default.xml'], function (xhr) {
+        // Adds bundle text to resources
+        mxResources.parse(xhr[0].getText());
+        // Configures the default graph theme
+        console.log(Graph.prototype.defaultThemeName)
+        themes[Graph.prototype.defaultThemeName] = xhr[1].getDocumentElement();
+        // Build viewer
+        window.viewer = new EditorUi(new Editor(true, themes), document.getElementById('viewer'), scheme);
+      },
+      function () {
+       console.log('Ошибка загрузки ресурсных файлов.');
+      });
+    };
+
+    const buildComplete = (result) => {
+      webix.ui(result);
+      buildTemplateComplete();
+    };
+
+    const buildPageLayout = (complete) => {
+
+      let schemeUI = webix.ui({
+        // id: 'contentview',
+        container: formRef.current,
+        type: 'clean',
+        template: "<div class='geViewer' id='viewer'></div>",
+      });
+      webix.require(
+        [
+          '/thirdparty/signalr/signalr.min.js',
+          'thirdparty/mxgraph/deflate/base64.js',
+          'thirdparty/mxgraph/deflate/pako.min.js',
+          'thirdparty/mxgraph/jscolor/jscolor.js',
+          'thirdparty/mxgraph/sanitizer/sanitizer.min.js',
+          'thirdparty/charts/Chart.bundle.min.js',
+          'thirdparty/charts/plugins/chartjs-plugin-annotation.min.js',
+          'thirdparty/charts/Chart.min.css',
+          'scripts/css/view.css',
+          'scripts/css/scheme.css',
+          'scripts/scada.js',
+          '/thirdparty/mxgraph/mxClient.min.js',
+          '/thirdparty/mxgraph/editor/Init.js',
+          '/thirdparty/mxgraph/editor/EditorUi.js',
+          '/thirdparty/mxgraph/editor/Editor.js',
+          '/thirdparty/mxgraph/editor/Sidebar.js',
+          '/thirdparty/mxgraph/editor/Graph.js',
+          '/thirdparty/mxgraph/editor/Shapes.js',
+          '/thirdparty/mxgraph/editor/Bindings.js',
+          '/thirdparty/mxgraph/editor/Actions.js',
+          '/thirdparty/mxgraph/editor/Menus.js',
+          '/thirdparty/mxgraph/editor/Dialogs.js',
+          '/thirdparty/mxgraph/editor/ViewMode.js',
+          '/thirdparty/mxgraph/editor/Scripts.js',
+          '/thirdparty/mxgraph/editor/MarksService.js',
+        ],
+        function () {
+          if (complete) complete(schemeUI);
+        }
+      );
+    };
+
+    const buildTemplateComplete = () => {
+      if (!window.mxClient) return;
+
+      if (!mxClient.isBrowserSupported()) {
+        console.log('Извините! Ваш браузер не поддерживается.');
+        return;
+      }
+
+      const LoadGraph = async () => {
+        const response = await  fetch('/assets/1x4kПС-1103510.txt')
+        const text = await response.text();
+
+        const r = {
+          id: '2251b1c0-f0a3-4979-99c3-caa251ff3cd2',
+          model: text,
+          name: '1x4kПС-1103510',
+          version: '3.0',
+        };
+        buildViewer.apply(window, [r]);
+      };
+      LoadGraph();
+    };
+
+    
+
+    buildPageLayout(buildComplete);
+    // return () => {
+    //   schemeUI.destructor();
+    // };
+   
+  }, []);
+
+  return <div id="layout" ref={formRef}> </div>;
+};
 
 export default Scheme1x4kПС1103510
